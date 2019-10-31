e09d4ff10a03fb57b5306653771134ed89aa0598
Justin Hutsell <hutselljustin@gmail.com>
2019-10-31 16:06:03 -0400

    adds fib example to hash

--- hashTables/hashes.js
+++ hashTables/hashes.js
@@ -16,3 +16,24 @@
 // index = hash % array_size
 // Perfect hashing allows for constant time lookups in all cases
 
+// An example of algorithm that can be better solved using a hash, is Fibonacci
+// In this case we are using memoization, which is "Is an optimization technique used primarily to speed up =>
+// computer programs by storing the results of expensive function calls."
+
+
+function fibonacci(num, memo) {
+    // If we store the value of each index in a hash, we can avoid the computational =>
+    // time of that value for the next N times; "caching" each value accordingly
+    memo = memo || {}
+    if (memo[num]) return memo[num];
+    // if the memo "object"(hash) contains a key equal to n, we return that related value
+    if (num <= 1) return num;
+    // since the fibonacci sequence is [0, 1, 1, 2, 3, 5...], if the number is 0 or 1, we will be returning that number
+
+    return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+    // If the memo hash doesn't contain a key equal to n, we calculate the fib number for the nth key and store it =>
+    // as the value for that nth key; Also return that value
+}
+
+// This method also utilizes recursion, which will be covered in another file
+
---
+++ pointerManipulation/variableAndPointerManipulation.js
@@ -0,0 +1,1 @@
+//  Variable/Pointer Manipulation
