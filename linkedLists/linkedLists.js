ec833c8b15f69515061d4c6e92da96ec07c78a73
Justin Hutsell <hutselljustin@gmail.com>
2019-11-05 16:33:09 -0500

    adds linked list description

---
+++ linkedLists/linkedLists.js
@@ -0,0 +1,8 @@
+// Linked Lists and their traversal
+
+// The linked list is a linear data structure, a collection of data elements(nodes), =>
+// in which each node points to the next 
+// Each node contains data, and a reference to the next node(only the next node in a singly linked list)
+// The benefits over something like an array, list elements can be easily inserted or removed => 
+// without reallocation of the entire structure, as the elements aren't physically stored in memory
+// Inherently accessed sequentially 
