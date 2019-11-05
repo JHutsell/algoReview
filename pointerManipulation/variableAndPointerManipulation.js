6e8e5b864242327b8182d77d31a1de91fb79ad03
Justin Hutsell <hutselljustin@gmail.com>
2019-11-05 15:34:29 -0500

    adds unique values example to dual pointers

--- pointerManipulation/variableAndPointerManipulation.js
+++ pointerManipulation/variableAndPointerManipulation.js
@@ -27,7 +27,7 @@ function twoSumMultiPointer(array, target) {
     let right = array.length - 1
 
     // Using a while loop to test if the left value is less than the right will ensure =>
-    // the entire array is transversed
+    // the entire array is traversed
     while (left < right) {
         let sum = array[left] + array[right]
         // if we find the target, we return it
@@ -43,6 +43,31 @@ function twoSumMultiPointer(array, target) {
     }
     // if we make it through the loop and we don't find our target, we can return false
     return false
+}
+
+// Another example is the problem of finding the amount of unique values in a sorted array
+// Note, the dual pointer method in this case only will work on a sorted array 
+
+function countUnique(array) {
+    if (array.length === 0) return 0;
+    // since we need to add 1 to the value of our first pointer to find our answer, we need to set this =>
+    // conditional to catch this odd case
+    let i = 0
+    // i will point to our first value, j our second to begin
+    for (let j = 1; j < array.length; j++) {
+        if (array[i] !== array[j]) {
+            // as we iterate we compare the values of array[i] and array[j]
+            i++;
+            // if the values are not equal, we've reached another unique value, hence increasing i 
+            array[i] = array[j];
+            // we also need to reassign i to the value of j in order to check that value against the remaining values
+            // in doing so, accounting for each unique value
+        }
+        // if the values are equal, we will only increment our value for j
+    }
+    return i + 1;
+    // in the end we will return our value for i + 1, as i was not only a pointer for an index in our array, =>
+    // but also a running count of the unique values, adding 1 to account for the index beggining at 0
 }
 
 // Another example of an algorithm that benefits from this technique is the longest palindrome =>
