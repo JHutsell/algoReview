c56bda5899d8bae45fd3d1b7c8031a4b3fa629d3
Justin Hutsell <hutselljustin@gmail.com>
2019-11-22 16:35:08 -0500

    adds description for bubble sort

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,5 @@
+// The most rudimentary sorting technique is bubble sort
+// The method repeatedly transverses the list and swaps elements if they =>
+// are in the incorrect order
+// It is names for the way elements "bubble" to the top of the list
+// Bubble sort is in most cases, slow and impractical 
