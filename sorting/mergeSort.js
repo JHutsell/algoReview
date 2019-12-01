20906017dedbf2db26351e91066e059994df183d
Justin Hutsell <hutselljustin@gmail.com>
2019-12-01 12:43:17 -0500

    adds more explanation for merge sort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,2 +1,6 @@
 // Another sorting method is known as "Merge Sort"
 // Merge sort is known by using a divide and conquer style sort
+
+// It is a comparison based sorting algorithm
+// The list is split into n sublists, where n is the number of elements
+// The sublists are repeatedly merged to produce new, sorted sublists
