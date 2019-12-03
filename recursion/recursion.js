8475d326ebac77ab27136716280790bd8b368977
Justin Hutsell <hutselljustin@gmail.com>
2019-12-03 15:53:32 -0500

    adds description for recursion

---
+++ recursion/recursion.js
@@ -0,0 +1,8 @@
+// Another useful technique in solving algorithms is the use of recursion
+// Recursion can be thought of as a procedure that occurs within itself
+// In terms of development, this can be thought of as a function that calls =>
+// itself within another function call 
+
+// The basic idea is that there are two properties, the base case and the recursive step
+// The base case is the scenario where the function will terminate, and will stop the recursion
+// The recursive step is the set of rules or processes that reduce each case towards the base case
